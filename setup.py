f64c25100d7a34c36217cc197ef25557db8e6dc0
paulc <paulc@>
2021-01-11 22:44:43 +0000

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(name='v6jail',
       version = '1.0',
       description = 'FreeBSD IPv6 Jail Management Utility',
-      install_requires = ['click==7.1.2','tabulate==0.8.7']
+      install_requires = ['click==7.1.2','tabulate==0.8.7'],
       url = 'https://github.com/paulc/v6jail',
       packages = ['v6jail'],
       license = 'BSD',
